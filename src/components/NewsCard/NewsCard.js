edf414a26ea1bbe342ba0b0444be9a0b4301c556
vamsi krishna <vamsikrishnaadusumalli999@gmail.com>
2020-08-11 23:12:31 +0530

    added refs for automatic scroll from Alan

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import React,{useState,useEffect,createRef} from 'react'
 import {Card,CardContent,CardMedia,CardActions,CardActionArea,Button,Typography}from '@material-ui/core';
 import useStyles from './styles.js'
+import classNames from 'classnames'
 
-const NewsCard = ({article:{description,publishedAt,source,title,url,urlToImage},i}) => {
+const NewsCard = ({article:{description,publishedAt,source,title,url,urlToImage},i,activeArticle}) => {
     const classes = useStyles()
+    const [elRefs,setElRefs] =useState([]);
+    const scrollToRef = (ref)=> window.scroll(0,ref.current.offsetTop-50);
+    //refs for scrolling
+    useEffect(()=>{
+        setElRefs((refs) => Array(20).fill().map((_ , j) => refs[j] || createRef()))
+    },[])
+
+    useEffect(()=>{
+        if(i===activeArticle&&elRefs[activeArticle]){
+            scrollToRef(elRefs[activeArticle])
+        }
+
+    },[i,activeArticle,elRefs])
+
     return (
        
-       <Card className={classes.card}>
+       <Card  ref={elRefs[i]} className={classNames(classes.card, activeArticle ===i ? classes.activeCard:null)}>
            <CardActionArea href={url} target="_blank">
                <CardMedia className={classes.media} image={urlToImage || ''}/>
                     <div className={classes.details}>
--- src/components/NewsCards/NewsCards.js
+++ src/components/NewsCards/NewsCards.js
@@ -10,7 +10,7 @@ const infoCards = [
     { color: '#283593', title: 'News by Sources', info: 'CNN, Wired, BBC News, Time, IGN, Buzzfeed, ABC News...', text: 'Give me the news from CNN' },
   ];
   
-const NewsCards = ({articles}) => {
+const NewsCards = ({articles,activeArticle}) => {
     const classes =  useStyles();
     if(!articles.length){
         return(
@@ -40,7 +40,7 @@ const NewsCards = ({articles}) => {
 
             { articles.map((article,i)=>(
                 <Grid key ={i} item xs ={12} sm={6} md={4} lg={3} style={{ display:'flex'}}>
-                      <NewsCard article={article} i={i}/>
+                      <NewsCard article={article} activeArticle={activeArticle} i={i}/>
                 </Grid>
             ))}
             </Grid>
