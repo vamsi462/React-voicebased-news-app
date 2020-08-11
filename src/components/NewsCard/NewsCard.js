49566e718d5e659cd0db0c8500542844eba609ae
vamsi krishna <vamsikrishnaadusumalli999@gmail.com>
2020-08-11 19:19:03 +0530

    news card with material UI

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,11 +1,27 @@
 import React from 'react'
-const NewsCard = () => {
+import {Card,CardContent,CardMedia,CardActions,CardActionArea,Button,Typography}from '@material-ui/core';
+
+const NewsCard = ({article:{description,publishedAt,source,title,url,urlToImage},i}) => {
     return (
        
-        <div>
-            news card
-        </div>
-       
+       <Card>
+           <CardActionArea>
+               <CardMedia />
+               <div>
+                   <Typography variant ="body2" color="textSecondary" component ="h2"></Typography>
+                   <Typography variant ="body2" color="textSecondary" component ="h2"></Typography>
+               </div>
+                    <Typography gutterBottom variant="h5"></Typography>
+                    <CardContent>
+                        <Typography variant="body2" color="textSecondary" component="p"></Typography>
+                    </CardContent>
+           </CardActionArea>
+           <CardActions>
+               <Button size="small" color="primary"></Button>
+                    <Typography variant="h5"  color="textSecondary"></Typography>
+           </CardActions>
+       </Card>
+
     )
 }
 
