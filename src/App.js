2391d2c6dbf994dd34aa9294de593989f70e90e3
vamsi krishna <vamsikrishnaadusumalli999@gmail.com>
2020-08-11 20:59:44 +0530

    visual look of the app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React,{useState,useEffect} from 'react'
 import alanBtn from "@alan-ai/alan-sdk-web"
 import NewsCards from './components/NewsCards/NewsCards'
-
+import useStyles from './styles'
 //alan key from alan app 
 const alanKey ='a0fd64217f6ed12e5ad06bc8633b86d92e956eca572e1d8b807a3e2338fdd0dc/stage'
 
 function App() {
   const [newsArticles,setNewsArticles] = useState([])
+  const classes = useStyles();
   useEffect(()=>{
     alanBtn({
       key:alanKey,
@@ -18,7 +19,9 @@ function App() {
   },[])
   return (
     <div>
-      <h1>ALan AI news app</h1>
+     <div className={classes.logoContainer}>
+        <img src="https://alan.app/voice/images/previews/preview.jpg" className={classes.alanLogo} alt="alan logo"/>
+     </div>
       <NewsCards articles ={newsArticles}/>
     </div>
   )
